31903589aec033dde927e4b9fc9e97dc47ca50a1
lianggaolin <1195391580@qq.com>
2015-12-07 17:55:09 +0800

    建立部分文件结构

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var express=require("express"),http=require("http");
+var express=require("express"),http=require("http"),path=require("path");
 var app=express();
 var server=http.createServer(app);
+//app.engine(".html",require("ejs").__renderFile);
 app.set("view engine","ejs");
 app.set("views","./views");
-app.use("static/",express.static("./static"));
-app.get(/\/(index(\.html)?)?/,function(req,res,next){
+app.use(express.static('static'));
+console.log(path.join(__dirname, 'static'));
+app.get(/\/index(\.html)?/,function(req,res,next){
 	res.status(200).render("index",{});
 })
-server.listen(3000);
 server.on("listen",function(){
 	console.log("开始监听");
 })
+server.listen(3000);
